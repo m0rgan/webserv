44e98c6cd06953e03e289e1704b50a2c045133d8
Miguel Moreno V. <miguelmoreno93@gmail.com>
2025-02-05 13:49:49 +0100

    minor change - deleted commets

--- include/ServerConfig.hpp
+++ include/ServerConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 12:59:38 by migumore          #+#    #+#             */
-/*   Updated: 2025/02/05 13:14:17 by migumore         ###   ########.fr       */
+/*   Updated: 2025/02/05 13:48:31 by migumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,21 @@ class ServerConfig
 public:
     std::vector<int> ports;
     std::string serverName;
-    std::map<int, std::string> errorPages; // 404 -> "/errors/404.html"
-    std::map<std::string, std::string> locations; // URI -> root directory
-    std::map<std::string, std::vector<std::string> > methods; // URI -> allowed methods
+    std::map<int, std::string> errorPages;
+    std::map<std::string, std::string> locations;
+    std::map<std::string, std::vector<std::string> > methods;
     std::string cgiExtension;
-
-    // New features
     std::vector<std::string> indexFiles;
     bool autoIndex;
     int maxBodySize;
     int clientTimeout;
     std::string logAccessFile;
     std::string logErrorFile;
-    std::map<std::string, std::string> redirects; // URI -> target
+    std::map<std::string, std::string> redirects;
     std::vector<std::string> upstreamServers;
     std::vector<std::string> allowList;
     std::vector<std::string> denyList;
-    std::map<std::string, std::string> mimeTypes; // Extension -> MIME Type
+    std::map<std::string, std::string> mimeTypes;
 
     ServerConfig();
     ~ServerConfig();
